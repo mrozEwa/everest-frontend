795ce40960350ebdee0d4f8c9aa8d97f2c66611a
“mrozEwa” <robert.aleks.zuk@gmail.com>
2025-04-27 10:42:07 +0200

    27.04.2025

--- app/turnusy-wytchnieniowe/components/TurnusyWrapper.jsx
+++ app/turnusy-wytchnieniowe/components/TurnusyWrapper.jsx
@@ -36,44 +36,37 @@ const TurnusyWrapper = ({ children }) => {
     }
   `;
   return (
-    <Box bg="white" maxW="full" pt={24} overflow="hidden">
-      {/* <Flex p={8} flex={1} align={"center"} justify={"center"}> */}
-      <Box
-        bgGradient="linear(to-t, white, blue.100)"
-        py={{ base: 10 }}
-        textAlign="center"
-        width="full"
-      >
-        <VStack spacing={6}>
-          <Heading
-            fontSize={{ base: "3xl", md: "4xl", lg: "5xl" }}
-            // animation={`${animation} .6s ease-in-out`}
+    <Box pt={28} pb={8} maxW={"7xl"} mx={"auto"}>
+      <Flex p={8} flex={1} align={"center"} justify={"center"}>
+        <Heading
+          fontSize={{ base: "3xl", md: "4xl", lg: "5xl" }}
+          animation={`${animation} .6s ease-in-out`}
+        >
+          <Text
+            as={"span"}
+            position={"relative"}
+            _after={{
+              content: "''",
+              width: "full",
+              height: useBreakpointValue({ base: "20%", md: "30%" }),
+              position: "absolute",
+              bottom: 1,
+              left: 0,
+              bg: "blue.400",
+              zIndex: -1,
+            }}
           >
-            <Text
-              as={"span"}
-              position={"relative"}
-              _after={{
-                content: "''",
-                width: "full",
-                height: useBreakpointValue({ base: "20%", md: "30%" }),
-                position: "absolute",
-                bottom: 1,
-                left: 0,
-                bg: "blue.400",
-                zIndex: -1,
-              }}
-            >
-              Turnusy wytchnieniowe –
-            </Text>
-            <br />{" "}
-            <Text color={"blue.400"} as={"span"}>
-              przestrzeń na oddech, odpoczynek i wspólnotę
-            </Text>{" "}
-          </Heading>
-        </VStack>
-      </Box>
-      {children}
-      {/* </Flex> */}
+            Turnusy wytchnieniowe –
+          </Text>
+          <br />{" "}
+          <Text color={"blue.400"} as={"span"}>
+            przestrzeń na oddech, odpoczynek i wspólnotę
+          </Text>{" "}
+        </Heading>
+      </Flex>
+      <Flex wrap="wrap" rowGap={8} justify="space-around">
+        {children}
+      </Flex>
     </Box>
   );
 };
