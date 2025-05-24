cf95fc114a07a77ac8a7bf1c280eccd933adb541
“mrozEwa” <robert.aleks.zuk@gmail.com>
2025-05-24 08:03:58 +0200

    add new Status

--- app/fundacja/page.jsx
+++ app/fundacja/page.jsx
@@ -1,6 +1,7 @@
 import getSlider from "@/lib/getSlider";
 import getDocs from "@/lib/getDocs";
 import getContent from "@/lib/getContent";
+import getStatus from "@/lib/getStatus";
 
 import Carousel from "@/app/fundacja/components/Carousel";
 import About from "@/app/fundacja/components/About";
@@ -10,12 +11,13 @@ export default async function page() {
   const slider = await getSlider();
   const docs = await getDocs();
   const content = await getContent();
+  const status = await getStatus();
 
   return (
     <div>
       <About content={content} />
       <Carousel images={slider} />
-      <Docs docs={docs} />
+      <Docs docs={docs} status={status} />
     </div>
   );
 }
